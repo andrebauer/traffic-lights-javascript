55bee6ed11f67e96e8ce83aafbbcd796bdc6fc18
André Bauer <a.bauer@ovm-kassel.de>
2019-05-29 16:37:24 +0200

    refine whitelistPatterns.

--- postcss.config.js
+++ postcss.config.js
@@ -9,7 +9,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
     // Include any special characters you're using in this regular expression
     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    whitelistPatterns: [/h-[0-9]+/g]
+    whitelistPatterns: [/^h-[0-9]+/g]
 })
 
 module.exports = {
